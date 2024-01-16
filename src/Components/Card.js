a518cd25413724aa37a8d490e7605162d1186acb
kinshuk-code-1729 <kinshuk2002.king@gmail.com>
2024-01-16 23:57:49 +0530

    post image added

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -19,6 +19,11 @@ const Card = () => {
                             <img className="float-end fs-3 p-2 mt-2" alt="more action" src={moreAction} />
                         </div>
                     </div>
+                    <div className="row">
+                        <div className="col-12">
+                            <img style={{borderRadius: '15px'}} className="p-2 img-fluid" alt="post image" src="https://images.unsplash.com/photo-1445543949571-ffc3e0e2f55e?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8d2ludGVyfGVufDB8fDB8fHww" />
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
