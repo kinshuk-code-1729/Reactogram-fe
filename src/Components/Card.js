b12d30e36bfca2fedceb0d6d741da06061a83f83
kinshuk-code-1729 <kinshuk2002.king@gmail.com>
2024-07-25 23:14:26 +0530

    card and navbar upgraded

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -21,7 +21,22 @@ const Card = () => {
                     </div>
                     <div className="row">
                         <div className="col-12">
-                            <img style={{borderRadius: '15px'}} className="p-2 img-fluid" alt="post image" src="https://images.unsplash.com/photo-1445543949571-ffc3e0e2f55e?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8d2ludGVyfGVufDB8fDB8fHww" />
+                            <img style={{ borderRadius: '15px' }} className="p-2 img-fluid" alt="post" src="https://images.unsplash.com/photo-1445543949571-ffc3e0e2f55e?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8d2ludGVyfGVufDB8fDB8fHww" />
+                        </div>
+                    </div>
+                    <div className="row my-3">
+                        <div className="col-6 d-flex">
+                            <i class="fa-regular fa-heart fs-4 ps-2"></i>
+                            <i class="fa-regular fa-comment fs-4 ps-3"></i>
+                            <i class="fa-solid fa-location-arrow fs-4 ps-3"></i>
+                        </div>
+                        <div className="col-6">
+                            <span className='pe-2 fs-6 fw-bold float-end'>100 likes</span>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col-12">
+                            <span className="p-2 text-muted">2 Hours Ago</span>
                         </div>
                     </div>
                 </div>
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -7,14 +7,14 @@ const Navbar = () => {
         <div>
             <nav className="navbar bg-light shadow-sm">
                 <div className="container-fluid">
-                    <a className="navbar-brand ms-5">
+                    <a href='/#' className="navbar-brand ms-5">
                         <img alt="logo" src={logo} height="45px" />
                     </a>
                     <form className="d-flex" role="search">
                         <input className="searchbox form-control me-2 text-muted" type="search" placeholder="Search" />
-                        <a className="nav-link text-dark fs-5" href="#"><i className="fa-solid fa-house mx-2"></i></a>
-                        <a className="nav-link text-dark fs-5" href="#"><i className="fa-regular fa-heart mx-2"></i></a>
-                        <a className="nav-link text-dark fs-5" href="#"><i className="fa-solid fa-circle mx-2"></i></a>
+                        <a className="nav-link text-dark fs-5" href="/#"><i className="fa-solid fa-house mx-2"></i></a>
+                        <a className="nav-link text-dark fs-5" href="/#"><i className="fa-regular fa-heart mx-2"></i></a>
+                        <a className="nav-link text-dark fs-5" href="/#"><i className="fa-solid fa-circle mx-2"></i></a>
                     </form>
                 </div>
             </nav>
